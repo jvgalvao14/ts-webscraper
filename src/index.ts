fb758223ad5f0a245351c62cbb1d6aaa3c6a3755
Joao Vitor <jv.galvao14@gmail.com>
2021-09-13 18:56:00 -0300

    Trying to connect to the MongoDB server

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,58 @@
 import axios from "axios";
 import { JSDOM } from "jsdom";
 var fs = require("fs");
+require("dotenv").config();
 
-let data = {
-    table: [],
-};
+//database stuff
+import { Schema, model, connect } from "mongoose";
 
-async function writeDataOnJson(number, extNumber) {
-    data.table.push({
-        extNumber: extNumber,
-        index: number,
+interface ExtensiveNumber {
+    extNumber: string;
+    index: string;
+}
+const schema = new Schema<ExtensiveNumber>({
+    extNumber: { type: String, required: true },
+    index: { type: String, required: true },
+});
+
+const extModel = model<ExtensiveNumber>("Number", schema);
+
+run().catch((err) => console.log(err));
+
+async function run(): Promise<void> {
+    await connect(process.env.URI);
+
+    const doc = new extModel({
+        extNumber: "Bill",
+        index: "bill@initech.com",
     });
-    let jsonData = JSON.stringify(data);
-    fs.writeFile("test.json", jsonData, function (err) {
-        if (err) {
-            console.log(err);
-        }
-    });
+    await doc.save();
+
+    console.log(doc.index);
 }
 
-async function getData() {
-    const index: number = 300;
-    for (let i = 0; i < index; i++) {
-        const url: string = `https://clevert.com.br/t/pt-br/numbers-to-words/index/pt-br/${i}`;
-        const data = (await axios.get(url)).data;
-        const { window } = new JSDOM(data);
-        const { document } = window;
+// async function writeDataOnDb(indexTag: string, extNumber: string) {
+//     await connect(process.env.URI);
 
-        let number = document.querySelector("input").value;
-        let extNumber = document.getElementById("resposta").innerHTML;
-        writeDataOnJson(number, extNumber);
-    }
-}
-getData();
+//     const doc = new extModel({
+//         extNumber: extNumber,
+//         index: indexTag,
+//     });
+
+//     await doc.save();
+// }
+
+// async function getData() {
+//     const index: number = 2;
+//     for (let i = 0; i < index; i++) {
+//         const url: string = `https://clevert.com.br/t/pt-br/numbers-to-words/index/pt-br/${i}`;
+//         const data = (await axios.get(url)).data;
+//         const { window } = new JSDOM(data);
+//         const { document } = window;
+
+//         let indexTag: string = document.querySelector("input").value;
+//         let extNumber: string = document.getElementById("resposta").innerHTML;
+//         writeDataOnDb(indexTag, extNumber);
+//     }
+// }
+// getData();
