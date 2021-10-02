40a4b7edafa3354a08d0e66ba264158e41808349
Joao Vitor <jv.galvao14@gmail.com>
2021-10-02 19:24:09 -0300

    Rolling the DB changes back for troubleshooting

--- src/index.ts
+++ src/index.ts
@@ -17,42 +17,42 @@ const schema = new Schema<ExtensiveNumber>({
 
 const extModel = model<ExtensiveNumber>("Number", schema);
 
-run().catch((err) => console.log(err));
+// run().catch((err) => console.log(err));
 
-async function run(): Promise<void> {
+// // async function run(): Promise<void> {
+// //     await connect(process.env.URI);
+
+// //     const doc = new extModel({
+// //         extNumber: "Bill",
+// //         index: "bill@initech.com",
+// //     });
+// //     await doc.save();
+
+// //     console.log(doc.index);
+// // }
+
+async function writeDataOnDb(indexTag: string, extNumber: string) {
     await connect(process.env.URI);
 
     const doc = new extModel({
-        extNumber: "Bill",
-        index: "bill@initech.com",
+        extNumber: extNumber,
+        index: indexTag,
     });
-    await doc.save();
 
-    console.log(doc.index);
+    await doc.save();
 }
 
-// async function writeDataOnDb(indexTag: string, extNumber: string) {
-//     await connect(process.env.URI);
+async function getData() {
+    const index: number = 30;
+    for (let i = 0; i < index; i++) {
+        const url: string = `https://clevert.com.br/t/pt-br/numbers-to-words/index/pt-br/${i}`;
+        const data = (await axios.get(url)).data;
+        const { window } = new JSDOM(data);
+        const { document } = window;
 
-//     const doc = new extModel({
-//         extNumber: extNumber,
-//         index: indexTag,
-//     });
-
-//     await doc.save();
-// }
-
-// async function getData() {
-//     const index: number = 2;
-//     for (let i = 0; i < index; i++) {
-//         const url: string = `https://clevert.com.br/t/pt-br/numbers-to-words/index/pt-br/${i}`;
-//         const data = (await axios.get(url)).data;
-//         const { window } = new JSDOM(data);
-//         const { document } = window;
-
-//         let indexTag: string = document.querySelector("input").value;
-//         let extNumber: string = document.getElementById("resposta").innerHTML;
-//         writeDataOnDb(indexTag, extNumber);
-//     }
-// }
-// getData();
+        let indexTag: string = document.querySelector("input").value;
+        let extNumber: string = document.getElementById("resposta").innerHTML;
+        console.log(indexTag, extNumber);
+    }
+}
+getData();
