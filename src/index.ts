bdcdb8f054177b59613d31943a2f2687e87b8964
Joao Vitor <jv.galvao14@gmail.com>
2021-10-12 16:39:13 -0300

    Heroku config

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ async function writeData(indexTag: string, extNumber: string) {
 }
 
 async function getData() {
-    const index: number = 30;
+    const index: number = 1000000;
     for (let i = 0; i < index; i++) {
         const url: string = `https://clevert.com.br/t/pt-br/numbers-to-words/index/pt-br/${i}`;
         const data = (await axios.get(url)).data;
